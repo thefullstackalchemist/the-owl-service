85c17766840d8be680bf8f03851dc119f62ac074
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-01-04 12:54:27 +0530

    ===> created an basic API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  // Check if the method is GET (or any other method you'd like to handle)
+  if (req.method === 'GET') {
+    res.status(200).json({ status: 'ok' , message: 'Hello from healthy API!' });
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
 }
