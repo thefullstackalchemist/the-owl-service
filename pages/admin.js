5976228c44f5738fdc8ee550a655a2ca2c2520c5
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-01-23 16:47:39 +0530

    delteold files

--- pages/admin.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-    return (
-      <div className="neuromorphic h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Admin</h1>
-      </div>
-    );
-  }
-  
--- pages/dashboard.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-    return (
-      <div className="neuromorphic h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold text-black">Dashboard</h1>
-      </div>
-    );
-  }
-  
