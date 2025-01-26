feba807e14e54d568e134ac26ee923e5d8f362b0
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-01-26 15:20:52 +0530

    UPDATE:  Automate DB create Process

--- src/services/DBManager.ts
+++ src/services/DBManager.ts
@@ -1,17 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// import sqlite3 from "sqlite3";
-// const filename : string = process.env.DB_FILE_PATH ?? '';
-// const db = new sqlite3.Database(filename, sqlite3.OPEN_READWRITE	);
-
 import sqlite3 from "sqlite3";
+import fs from "fs";
+import os from "os";
 
 class DatabaseManager {
     private static instance: DatabaseManager;
     private db: sqlite3.Database;
 
     private constructor() {
-        const filename: string = process.env.DB_FILE_PATH ?? '';
-        console.dir(filename);
+        const filename: string = process.env.DB_FILE_PATH ?? './database.db';
+
+        // Check and create file if it doesn't exist
+        if (!fs.existsSync(filename)) {
+            try {
+                fs.writeFileSync(filename, "");
+                console.log("Database file created:", filename);
+
+                // Set permissions for Linux
+                if (os.platform() === "linux") {
+                    fs.chmodSync(filename, 0o777);
+                    console.log("Set 777 permissions for the database file on Linux.");
+                }
+            } catch (err) {
+                console.error("Error creating database file:", err.message);
+                throw err;
+            }
+        }
+
+        // Initialize the database connection
         this.db = new sqlite3.Database(filename, sqlite3.OPEN_READWRITE, (err) => {
             if (err) {
                 console.error("Error opening database:", err.message);
@@ -51,7 +66,4 @@ class DatabaseManager {
     }
 }
 
-// const query = "SELECT * FROM users WHERE id = ?";
-// const params = [1];
-
-export default DatabaseManager
+export default DatabaseManager;
